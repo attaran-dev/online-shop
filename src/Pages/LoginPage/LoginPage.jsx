518f915e311f195ed2cb12316281f74cfdf96de7
attaran-dev <103171587+attaran-dev@users.noreply.github.com>
2022-08-14 20:43:27 +0430

    add login page

---
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <main className='flex justify-center items-center min-h-screen'>
+        <form action="" className='flex flex-col w-50 p-6 gap-2 shadow-2xl'>
+            <label htmlFor="username">نام کاربری:</label>
+            <input type="text" name="username" id="username" className='p-2 border-b-2' />
+            <label htmlFor="password">کلمه‌ی عبور:</label>
+            <input type="password" name="password" id="password" className='p-2 border-b-2' />
+            <button type="submit" className='bg-gray-100 py-2'>ورود</button>
+        </form>
+    </main>
+  )
+}
+
+export default LoginPage
